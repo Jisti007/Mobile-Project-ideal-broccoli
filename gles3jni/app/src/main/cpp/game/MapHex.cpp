b27e4c24b0d6483aa3c696bf54830face9fceaff
Jisti007 <tammi_574@hotmail.com>
2017-09-14 15:54:42 +0300

    wip map

--- gles3jni/app/src/main/cpp/game/MapHex.cpp
+++ gles3jni/app/src/main/cpp/game/MapHex.cpp
@@ -1,1 +1,13 @@
 #include "MapHex.h"
+#include "math.h"
+#include <GLES3/gl3.h>
+
+const float hex_r = 0.2;
+const float hex_dx = hex_r*cos(30.0*M_PI/180.0);
+const float hex_dy = hex_r*sin(30.0*M_PI/180.0);
+const float hex_gx = 2.0*hex_dx;
+const float hex_gy = 2.0*hex_dx*sin(60.0*M_PI/180.0);
+
+void Hex(float x, float y, float z) {
+
+}
