94892b538e59264e840ef52172016c79c4f3a6cb
Jisti007 <tammi_574@hotmail.com>
2017-12-11 16:40:32 +0200

    line breaking, proper infobox

--- gles3jni/app/src/main/cpp/game/ui/InfoPanel.cpp
+++ gles3jni/app/src/main/cpp/game/ui/InfoPanel.cpp
@@ -21,7 +21,7 @@ void InfoPanel::updateInfo(Unit* unit) {
 	auto unitIcon = addNewChild<UISprite>(
 		unitUISprite,
 		Transform2D{
-			Translation{0 - infoPanelSprite->getWidth() / 3, 0},
+			Translation{0 - infoPanelSprite->getWidth() / 3 + unitUISprite->getSize().x / 3.0f, 0},
 			Scale{2.4f, 2.4}
 		}
 	);
@@ -32,48 +32,20 @@ void InfoPanel::updateInfo(Unit* unit) {
 	auto defense = unitType->getDefense();
 	auto movement = unitType->getMovement();
 
-	std::stringstream nameString;
-	nameString << unitName;
+	std::stringstream infoString;
+	infoString << unitName
+		<< "\nHP: " << currentHP << "/" << maxHP
+		<< "\nDefense: " << defense
+		<< "\nMovement points: " << movement << "\0";
 
-	nameLabel = addNewChild<Label>(
-		nameString.str().c_str(),
+	infoLabel = addNewChild<Label>(
+		infoString.str().c_str(),
 		font,
 		glm::vec2{0,infoPanelSprite->getHeight() / 3.0f},
-		glm::vec2{100,20}
+		glm::vec2{infoPanelSprite->getWidth() / 2.0f,infoPanelSprite->getHeight() / 5.0f}
 	);
 
-	std::stringstream hpString;
-	hpString << "HP: " << currentHP << "/" << maxHP;
-
-	hpLabel = addNewChild<Label>(
-		hpString.str().c_str(),
-		font,
-		glm::vec2{0, 0 + 100},
-		glm::vec2{100,20}
-	);
-
-	std::stringstream defenseString;
-	defenseString << "Defense: " << defense;
-
-	defenseLabel = addNewChild<Label>(
-		defenseString.str().c_str(),
-		font,
-		glm::vec2{0, hpLabel->getBottom() - hpLabel->getSize().y},
-		glm::vec2{100,20}
-	);
-
-	std::stringstream movementString;
-	movementString << "Movement: " << movement;
-
-	movementLabel = addNewChild<Label>(
-		movementString.str().c_str(),
-		font,
-		glm::vec2{0, defenseLabel->getBottom() - defenseLabel->getSize().y},
-		glm::vec2{100,20}
-	);
-
-	std::stringstream skillString;
-	int skillOffset = 30;
+	int skillOffset = 0;
 
 	for (auto& skill : unitType->getSkills()){
 
@@ -81,33 +53,25 @@ void InfoPanel::updateInfo(Unit* unit) {
 		auto skillIcon = addNewChild<UISprite>(
 			skillSprite,
 			Transform2D{
-				Translation{0, movementLabel->getBottom() - movementLabel->getSize().y - skillOffset},
+				Translation{0, infoLabel->getBottom() - infoLabel->getSize().y - skillSprite->getSize().y / 3.0f - skillOffset},
 				Scale{0.75f, 0.75f}
 			}
 		);
 
-		//TODO: Make this a bit smoother
-		/*auto name = skill->getName();
-		auto description = skill->getDescription();
-		auto cost = skill->getCost();
-		auto range = skill->getRange();
-		auto effects = skill->getEffects();
-		auto target = skill->getTargetType();*/
+		std::stringstream skillString;
+		skillString << skill->getName() << "\n"
+					<< skill->getDescription() << "\n"
+					<< "Cost: " << skill->getCost() << "\n"
+					<< "Range: " << skill->getRange();
 
-		newStringLabel(skill->getName(), -30 + skillOffset);
-		newStringLabel(skill->getDescription(), -10 + skillOffset);
-		newStringLabel("Cost: ", skill->getCost(), 10 + skillOffset);
-		newStringLabel("Range: ", skill->getRange(), 30 + skillOffset);
-
-		/*skillString << skillName;
-		auto skillLabel = addNewChild<Label>(
+		skillLabel = addNewChild<Label>(
 			skillString.str().c_str(),
 			font,
-			glm::vec2{skillIcon->getPosition().x + skillIcon->getSize().x / 2.0f, movementLabel->getBottom() - movementLabel->getSize().y - skillOffset},
-			glm::vec2{100,20}
-		);*/
+			glm::vec2{assetManager->getSprite("attack")->getWidth() / 2.0f, infoLabel->getBottom() - infoLabel->getSize().y - skillOffset},
+			glm::vec2{infoPanelSprite->getWidth() / 2.0f,300}
+		);
 
-		skillOffset += skillIcon->getSize().y;
+		skillOffset += skillLabel->getSize().y / 2.0f;
 
 		skillString.str(std::string());
 	}
@@ -135,58 +99,24 @@ void InfoPanel::updateInfo(Building* building) {
 	auto buildingIcon = addNewChild<UISprite>(
 		buildingUISprite,
 		Transform2D{
-			Translation{0 - infoPanelSprite->getWidth() / 3, 0},
-			Scale{2.4f, 2.4f}
+			Translation{0 - infoPanelSprite->getWidth() / 3 + buildingUISprite->getSize().x / 3.0f, 0},
+			Scale{2.4f, 2.4}
 		}
 	);
 
 	auto buildingName = buildingType->getName();
 	auto buildingDescription = buildingType->getDescription();
 
-	std::stringstream nameString;
-	nameString << buildingName;
+	std::stringstream infoString;
+	infoString << buildingName << "\n"
+				<< buildingDescription;
 
-	nameLabel = addNewChild<Label>(
-		nameString.str().c_str(),
+	infoLabel = addNewChild<Label>(
+		infoString.str().c_str(),
 		font,
 		glm::vec2{0,infoPanelSprite->getHeight() / 3.0f},
-		glm::vec2{100,20}
+		glm::vec2{infoPanelSprite->getWidth() / 2.0f,300}
 	);
-
-	std::stringstream descriptionString;
-	descriptionString << buildingDescription;
-
-	/*descriptionLabel = addNewChild<Label>(
-		descriptionString.str().c_str(),
-		font,
-		glm::vec2{0,infoPanelSprite->getHeight() / 3.0f - nameLabel->getSize().y},
-		glm::vec2{100,20}
-	);*/
-
-	/*
-	std::stringstream skillString;
-	int skillOffset = 30;
-
-	for (auto& skill : unitType->getSkills()){
-
-		auto skillSprite = skill->getSprite();
-		auto skillIcon = addNewChild<UISprite>(
-			skillSprite,
-			glm::vec2{0,movementLabel->getBottom() - movementLabel->getSize().y - skillOffset},
-			0.75f
-		);
-
-		//TODO: Make this a bit smoother
-
-		newStringLabel(skill->getName(), -30 + skillOffset);
-		newStringLabel(skill->getDescription(), -10 + skillOffset);
-		newStringLabel("Cost: ", skill->getCost(), 10 + skillOffset);
-		newStringLabel("Range: ", skill->getRange(), 30 + skillOffset);
-
-		skillOffset += skillIcon->getSize().y;
-
-		skillString.str(std::string());
-	}*/
 
 	auto exitButtonSprite = assetManager->getSprite("no_button");
 	auto exitButton = addNewChild<Button>(
@@ -196,37 +126,4 @@ void InfoPanel::updateInfo(Building* building) {
 	);
 	//exitButton->setOnPress(std::bind(&Game::popState, game));
 
-}
-
-void InfoPanel::newStringLabel(std::string textString, int skillOffset) {
-	std::stringstream skillString;
-	skillString << textString;
-	auto skillLabel = addNewChild<Label>(
-		skillString.str().c_str(),
-		font,
-		glm::vec2{assetManager->getSprite("attack")->getWidth() / 2.0f, movementLabel->getBottom() - movementLabel->getSize().y - skillOffset},
-		glm::vec2{100,20}
-	);
-}
-
-void InfoPanel::newStringLabel(std::string textString, int value, int skillOffset) {
-	std::stringstream skillString;
-	skillString << textString << value;
-	auto skillLabel = addNewChild<Label>(
-		skillString.str().c_str(),
-		font,
-		glm::vec2{assetManager->getSprite("attack")->getWidth() / 2.0f, movementLabel->getBottom() - movementLabel->getSize().y - skillOffset},
-		glm::vec2{100,20}
-	);
-}
-
-void InfoPanel::newStringLabel(std::string textString, float value, int skillOffset) {
-	std::stringstream skillString;
-	skillString << textString << value;
-	auto skillLabel = addNewChild<Label>(
-		skillString.str().c_str(),
-		font,
-		glm::vec2{assetManager->getSprite("attack")->getWidth() / 2.0f, movementLabel->getBottom() - movementLabel->getSize().y - skillOffset},
-		glm::vec2{100,20}
-	);
 }
--- gles3jni/app/src/main/cpp/game/ui/Label.cpp
+++ gles3jni/app/src/main/cpp/game/ui/Label.cpp
@@ -10,14 +10,30 @@ void Label::onDraw(Pipeline* pipeline) {
 	size_t i = 0;
 	//int maxRowHeight = 0;
 	glm::vec2 offset = getPosition();
+	originalPosition = offset;
 
 	while(text[i] != '\0') {
-		char c = text[i];
+		if (text[i] == '\n') {
 
-		Sprite* sprite = font->getCharacter(c).sprite;
-		pipeline->draw(sprite, offset);
-		offset.x += sprite->getWidth()+5;
-		i++;
+			/*char c = text[i];
+			Sprite* sprite = font->getCharacter(c).sprite;
+			pipeline->draw(sprite, offset);*/
+			offset.x = originalPosition.x;
+			offset.y -= lastSprite->getHeight()+5;
+			i++;
+			} else {
+			char c = text[i];
+
+			Sprite* sprite = font->getCharacter(c).sprite;
+			pipeline->draw(sprite, offset);
+			offset.x += sprite->getWidth()+5;
+			lastSprite = sprite;
+			if (offset.x > this->getSize().x && text[i] != ' '){
+				offset.x = originalPosition.x;
+				offset.y -= lastSprite->getHeight()+5;
+			}
+			i++;
+		}
 	}
 
 
